0b2ff5701747f112852eb08878c0bd73003c2e3a
- <a-@192.168.1.25>
2021-08-05 17:23:00 +0700

    tambah sedikit

--- mysite/task/views.py
+++ mysite/task/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
+from . import models
 
 
 # Create your views here.
 
-def index(req):   
-    if req.POST:
-            yourname = req.POST['name']
-            print(yourname)
-            yourpassword = req.POST['password']
-            print(yourpassword)
-    return render(req, 'index.html')
+def index(request):   
+    data = models.tugas.objects.all()
+    contoh ="welcome back"
+    if request.POST:
+            input_nama = request.POST['name']
+            models.tugas.objects.create(name=input_nama)
+            data = models.tugas.objects.all()
+    return render(request, 'index.html', {
+        'data': data,
+    })
 
 def about(req):
     return render(req, 'about.html')
