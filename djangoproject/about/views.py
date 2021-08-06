99b1e36860c7d7a1a087dd094002494294f488c9
dabgetup <prastowoteguhw@gmail.com>
2021-08-06 21:11:00 +0700

    add static about

--- djangoproject/about/views.py
+++ djangoproject/about/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context = {
+        'judul' :'about',
+        'subjudul' :'ini adalah tentang kita bersama',
+    }    
+    return render(request, 'about/index.html', context)
+
+
