e465acd43fb0db38cd1b371b6143f28155afc2b1
dabgetup <prastowoteguhw@gmail.com>
2021-08-06 17:49:57 +0700

    update

--- mysite/task/urls.py
+++ mysite/task/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about),
+    path('update/<id>', views.update),
+    path('delete/<id>', views.index),
     
 ]
--- mysite/task/views.py
+++ mysite/task/views.py
@@ -8,12 +8,29 @@ def index(request):
     data = models.tugas.objects.all()
     contoh ="welcome back"
     if request.POST:
-            input_nama = request.POST['name']
-            models.tugas.objects.create(name=input_nama)
-            data = models.tugas.objects.all()
+        input_nama = request.POST['name']
+        models.tugas.objects.create(name=input_nama)
+        data = models.tugas.objects.all()
     return render(request, 'index.html', {
         'data': data,
     })
 
 def about(req):
     return render(req, 'about.html')
+
+def update(request, id):
+    if request.POST:
+        newname = request.POST['username']
+        models.tugas.objects.filter(pk=id). update(name=new_name)
+        redirect('/')
+    return render(request, 'index.html',{
+        'data': data,
+    })
+
+def delete(request, id):
+    return render(request, 'index.html',{
+        'data': data,
+    })
+
+
+
